e439b54475e92cfec64af807f56fab6948b22730
githubMyoThant <myothantdev@gmail.com>
2022-11-05 12:12:37 +0630

    Weather

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Weather App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -30,44 +31,94 @@ class Weather extends StatefulWidget {
 }
 
 class _WeatherState extends State<Weather> {
+  getWeather() async {
+    var city = await WeatherApi().getWeatherCity(city_name.text);
+    setState(() {
+      temperature = city['main']['temp'].toString();
+      condition = city['weather'][0]['description'];
+      cityName = city['name'];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getWeather();
+  }
+
   TextEditingController city_name = TextEditingController();
   var temperature = "";
   var condition = "";
-  var cityName = "";
-  var lat = "";
-  var lon = "";
+  var cityName = "Mogok";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      body: Column(
-        children: [
-          TextField(
-            controller: city_name,
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(
+                    "https://www.survivingwithandroid.com/wp-content/uploads/2014/11/android_weather_app.jpg"),
+                fit: BoxFit.cover)),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 45, bottom: 45),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: 350,
+                          child: TextField(
+                            onChanged: (value) => getWeather(),
+                            controller: city_name,
+                            decoration: const InputDecoration(
+                              labelText: "City Name",
+                              border: OutlineInputBorder(),
+                              suffixIcon: Icon(Icons.search),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: IconButton(
+                              onPressed: () {},
+                              icon: const Icon(Icons.gps_fixed_outlined)),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Row(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 5.0),
+                        child: Icon(Icons.location_pin),
+                      ),
+                      Text(
+                        cityName,
+                        style: const TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.w400),
+                      ),
+                    ],
+                  ),
+                  Text(condition),
+                  Text(temperature),
+                  // Image.asset('images/bg.webp')
+                ],
+              )
+            ],
           ),
-          ElevatedButton(
-              onPressed: () async {
-                var city = await WeatherApi().getWeatherCity(city_name.text);
-                setState(() {
-
-                  temperature = city['main']['temp'].toString();
-                  condition = city['weather'][0]['description'];
-                  cityName = city['name'];
-                  // lon = city['coord']['lon'];
-                  // lat = city['coord']['lat'];
-                });
-              },
-              child: const Text("Search")),
-              // ElevatedButton(onPressed: (() async{
-              //   var city = await WeatherApi().getWeatherLatLon(lat, lon);
-              // }), child: const Text("Search with latitude and longitude")),
-          Text(temperature),
-          Text(condition),
-          Text(cityName),
-        ],
+        ),
       ),
     );
   }
